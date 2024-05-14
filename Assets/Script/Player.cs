5432eddcf3bec87f3177fbd6fb07ccfb2b97e00b
SPHLam <samphanhoailam@gmail.com>
2024-05-14 18:55:19 +0700

    Implement Player Jump functionality
    - Using a RaycastHit2D = Physics2D.Raycast with the down direction to detect if the Player is on the Ground (hit the Ground)

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     // Get handle to rigid body
     private Rigidbody2D _rigidBody2D;
+    private float _jumpForce = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,39 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+
+        Move();
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround())
+        {
+            Jump();
+        }
+    }
+
+    private bool isOnGround()
+    {
+        // Raycast to detect if the Player is still on the ground or not
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.75f, 1 << 8); // Bit shift to find the layer value
+
+        if (hitInfo.collider != null) // hit something below, aka the ground
+        {
+            // Debug.Log("Hit: " + hitInfo.collider.name);
+            // Debug.DrawRay(transform.position, Vector2.down * 0.75f, Color.green);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Jump()
+    {
+        _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x, _jumpForce);
+    }
+
+    private void Move()
     {
         // Horizontal input for left/right
         float horizontalInput = Input.GetAxisRaw("Horizontal");
