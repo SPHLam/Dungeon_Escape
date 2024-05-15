68c2c01d393b791f9a4f235a67a75fe14abf3100
SPHLam <samphanhoailam@gmail.com>
2024-05-15 23:29:22 +0700

    Implement Moss Giant Animation
    - Implement Animation logic for Idle and Walking
    - Stop when Idling using if(_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) return; before moving function

--- Assets/Script/Enemy/MossGiant.cs
+++ Assets/Script/Enemy/MossGiant.cs
@@ -7,11 +7,14 @@ public class MossGiant : Enemy
 {
     private Vector3 _targetPosition;
     private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private bool _isFlipped = false;
     private void Start()
     {
         speed = 2;
         _targetPosition = pointB.position;
         _animator = GetComponentInChildren<Animator>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void Attack()
@@ -21,7 +24,12 @@ public class MossGiant : Enemy
 
     public override void Update()
     {
-
+        // if idle animation is playing
+        // do nothing -> hint: use return
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
         Move();
     }
 
@@ -34,25 +42,25 @@ public class MossGiant : Enemy
         // ** cant use else becuz the position between 2 way points
         float step = speed * Time.deltaTime;
 
+        _spriteRenderer.flipX = _isFlipped;
+
         transform.position = Vector3.MoveTowards(transform.position, _targetPosition, step);
 
         if (Vector3.Distance(transform.position, _targetPosition) < 0.001f)
         {
-            StartCoroutine(StopAndIdling());
             _animator.SetTrigger("Idle");
 
             if (_targetPosition == pointB.position)
+            {
+                _isFlipped = true;
                 _targetPosition = pointA.position;
+            }
+                
             else if (_targetPosition == pointA.position)
+            {
+                _isFlipped = false;
                 _targetPosition = pointB.position;
-        }
-    }
-
-    IEnumerator StopAndIdling()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(4.5f);
+            }
         }
     }
 }
